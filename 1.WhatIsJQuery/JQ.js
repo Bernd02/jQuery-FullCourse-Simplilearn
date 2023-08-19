6fc9ab541226ddc491755ddfdd9f6a576491c4dd
Bernd <141633738+Bernd02@users.noreply.github.com>
2023-08-19 17:27:32 +0200

    Finished - 2. jQuery Selectors

---
+++ 1.WhatIsJQuery/JQ.js
@@ -0,0 +1,9 @@
+import("../jQuery/jquery-3.7.0.js");
+
+// https://www.youtube.com/watch?v=Rkvn_MA04fo
+// 00:01:42
+// 1. What is jQuery
+function func1() {
+    $("#img1").fadeToggle(2000);
+}
+
---
+++ 2.jQuerySelectors/JQ.js
@@ -0,0 +1,24 @@
+import("../jQuery/jquery-3.7.0.js");
+
+// https://www.youtube.com/watch?v=Rkvn_MA04fo
+// 00:13:06
+// 2. jQuery Selectors
+function func1() {
+    // $("div").css("background-color", "orange");
+    // $("#p1").css("font-style", "italic");
+    // $(".para").css("font-style", "italic");
+    // $("#div1, #div3").css("background-color", "green");
+
+    // # li > alle li's
+    // $("#div1, li").css("background-color", "purple");
+
+    // $("div > p").fadeToggle();
+    // $("div p").fadeToggle();
+
+    // $("p:first").fadeToggle();
+
+    // $("li:even").fadeToggle();
+    $("li:odd").fadeToggle();
+}
+
+
