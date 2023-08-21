f18eef2296a7793abf3a739a55f33f94d0be9b7c
Bernd <141633738+Bernd02@users.noreply.github.com>
2023-08-21 19:57:12 +0200

    Finished - 11. ToDo List

--- 11.ToDoList/JQ.js
+++ 11.ToDoList/JQ.js
@@ -1,6 +1,18 @@
 // 04:24:15 > ToDo List
 
 $(() => {
+    $("#button").on("click", () => {
+        let input = $("input[name=list-item]");
+        let toAdd = input.val();
 
+        $("ul").append(`<li>${toAdd}</li>`);
+        input.val("");
+    });
 
+    // Deze functie wordt 1-malig geïnstantieerd
+    $(document).on("dblclick", "li", function (e) {
+        $(e.target).toggleClass("strike").fadeOut("slow");
+    });
+
+    $("ul").sortable();
 });
